1a19f57c3fd1b53af72bad7f73ba10ab752d69ab
chlek <chlek@Chandras.lan>
2024-07-02 23:29:54 -0400

    Undesrtanding of Git commit and pull.

---
+++ src/javatraining/UnderstaningOfGitCommands1.java
@@ -0,0 +1,10 @@
+package javatraining;
+
+public class UnderstaningOfGitCommands1 {
+
+	public static void main(String[] args) {
+		System.out.println("Commiting the code to master branch and will pull from local branch!");
+
+	}
+
+}
