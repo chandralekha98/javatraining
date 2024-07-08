73914754c56a6e38a8c8bdb456cfcd0eb86749fa
chlek <chlek@Chandras.lan>
2024-07-08 00:00:59 -0400

    Print name in native language

--- src/javatraining/A.java
+++ src/javatraining/A.java
@@ -3,5 +3,8 @@ package javatraining;
 class A {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int ln=1234_23;
+		byte b1=(byte)250;
+		System.out.println(b1);
 	}
 }
---
+++ src/javatraining/NameInNativeLanguage.java
@@ -0,0 +1,11 @@
+package javatraining;
+
+public class NameInNativeLanguage {
+
+	public static void main(String[] args) {
+		String teluguString = "\u0C1A\u0C02\u0C26\u0C4D\u0C30\u0C3E\u0C32\u0C47\u0C15\u0C3E";
+
+		System.out.println("Telugu string for chandralekha: " + teluguString);
+	}
+
+}
