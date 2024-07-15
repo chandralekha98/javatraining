78f271f6c2245512242620aad00513004fde73b0
chlek <chlek@Chandras.lan>
2024-07-14 22:19:54 -0400

    3 ways to write infinite loops

---
+++ src/javatraining/ThreewaysToInfiniteLoops.java
@@ -0,0 +1,17 @@
+package javatraining;
+
+public class ThreewaysToInfiniteLoops {
+
+	public static void main(String[] args) {
+		
+		while(true) {
+		}
+//		for (;;) {
+//		    // Code that executes indefinitely
+//		}
+//		do{
+//			
+//		}while(true);
+	}
+
+}
